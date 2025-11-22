7fb9364f747644fdef9688094d7bc9a97c7c818b
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-11-22 12:59:59 -0600

    feat: add user mock data for testing and development purposes

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,10 @@ const app = express(); // Crear una instancia de Express
 
 app.use(express.json());
 
+// Rutas
 app.use("/api/v1/users", userRouter); // Usar el enrutador de usuarios 
+
+// Ejemplo de endpoint:
+// http://localhost:4000/api/v1/users/crear
 
 export default app;
