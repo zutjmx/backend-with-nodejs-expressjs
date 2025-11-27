62210fc2c935b31e82ebc1294c86abfe43a2881c
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-11-27 12:59:04 -0600

    feat: add update and delete functionality for posts

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -28,3 +28,44 @@ export const getPosts = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+export const updatePost = async (req, res) => {
+    try {
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).json({ message: "Los datos para actualizar no pueden estar vacíos" });
+        }
+        
+        const { id } = req.params;
+        
+        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+            return res.status(400).json({ message: "ID de post inválido" });
+        }
+
+        const { name, description, age } = req.body;
+        const updatedPost = await Post.findByIdAndUpdate(id, { name, description, age }, { new: true });
+        
+        if (!updatedPost) {
+            return res.status(404).json({ message: "Post no encontrado" });
+        }
+
+        res.status(200).json({ message: "Post actualizado exitosamente", post: updatedPost });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const deletePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+            return res.status(400).json({ message: "ID de post inválido" });
+        }
+        const deletedPost = await Post.findByIdAndDelete(id);
+        if (!deletedPost) {
+            return res.status(404).json({ message: "Post no encontrado" });
+        }
+        res.status(200).json({ message: "Post eliminado exitosamente" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import { createPost, getPosts, updatePost,deletePost } from "../controllers/post.controller.js";
 
 const router = Router();
 
-router.route("/crear").post(createPost);
 router.route("/listar").get(getPosts);
+router.route("/crear").post(createPost);
+router.route("/actualizar/:id").put(updatePost);
+router.route("/borrar/:id").delete(deletePost);
 
 export default router;
